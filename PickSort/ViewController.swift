9a56182519717c3bc84ae62484ff36ded1b54490
Ashraf Alif Adillah <ashraf.alif@gmail.com>
2025-07-19 22:03:59 +0700

    Initial Commit

---
+++ PickSort/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PickSort
+//
+//  Created by Ashraf Alif Adillah on 19/07/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
