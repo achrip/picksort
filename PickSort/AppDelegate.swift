c6a267f6908a222afa6007540f3319671158959d
Ashraf Alif Adillah <ashraf.alif@gmail.com>
2025-07-20 22:42:48 +0700

    Add file selector

--- PickSort/AppDelegate.swift
+++ PickSort/AppDelegate.swift
@@ -7,24 +7,24 @@
 
 import Cocoa
 
-@main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     
-
-
+    private var windowController: NSWindowController?
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        self.windowController = WindowController()
+        windowController?.showWindow(self)
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+    
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-
-
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+            return true
+        }
 }
-
